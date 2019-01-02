faaa52706217abcc869bc0658c9658481853d6a1
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-01-02 18:01:44 +0800

    implement reminder and alert for urgent and overdue tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,8 @@ class TasksController < ApplicationController
   def index
     @archived = Task.archived.order(order)
     @ongoing = Task.ongoing.order(order)
+    add_reminder_for_urgent
+    add_alert_for_overdue
   end
 
   private
@@ -15,6 +17,36 @@ class TasksController < ApplicationController
       'title ASC'
     else
       'created_at DESC'
+    end
+  end
+
+  # count urgent tasks
+  def urgent_count
+    @ongoing.count &:urgent?
+  end
+
+  # count overdue tasks
+  def overdue_count
+    @ongoing.count &:overdue?
+  end
+
+  # add a flash reminder message if there are urgent tasks
+  def add_reminder_for_urgent
+    _urgent_count = urgent_count
+    if _urgent_count.positive?
+    flash.now[:reminder] = "#{_urgent_count}
+      #{_urgent_count == 1 ? 'task is' : 'tasks are'}
+      due within one day."
+    end
+  end
+
+  # add a flash alert message if there are overdue tasks
+  def add_alert_for_overdue
+    _overdue_count = overdue_count
+    if _overdue_count.positive?
+      flash.now[:alert] = "#{_overdue_count}
+      #{_overdue_count == 1 ? 'task is' : 'tasks are'}
+      overdue"
     end
   end
 end
