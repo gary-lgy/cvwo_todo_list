18e423e25dbf099319fd8ce078e78ee9317f7a70
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-01-03 01:02:38 +0800

    Revert "order tasks by time created by default"
    This reverts commit 77a01f0785ec8c75ed3b5ba12d9362090330c4f7.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: redirect('/tasks?order=1')
+  get '/', to: redirect('/tasks')
   resources :tasks do
     get 'toggle_completed', on: :member
   end
