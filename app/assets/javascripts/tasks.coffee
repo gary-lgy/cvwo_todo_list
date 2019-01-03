67e6759d256744b3dd02d9b64bf12d242403141f
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-01-03 23:38:32 +0800

    implement adding and removing tags from a task

--- app/assets/javascripts/tasks.coffee
+++ app/assets/javascripts/tasks.coffee
@@ -1,3 +1,14 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on 'turbolinks:load', ->
+  jQuery ->
+    $('form').on 'click', '#add_tag', (event) ->
+      $(this).before($(this).data('fields'))
+      event.preventDefault()
+
+    $('form').on 'click', '#remove_tag', (event) ->
+      $(this).prev('input[type=hidden]').val('1')
+      $(this).closest('div[class=tag]').hide()
+      event.preventDefault()
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -42,4 +42,31 @@ module TasksHelper
       'created_at DESC'
     end
   end
+
+  # generate a link to dynamically add a tag when editing a task
+  def link_to_add_tag
+    fields = content_tag :div, class: 'tag' do
+      text_field_tag 'task[tags][][name]'
+    end
+    link_to 'Add Tag',
+            '#',
+            id: 'add_tag',
+            data: { fields: fields.delete("\n") }
+  end
+
+  # add or remove tags as required
+  def add_or_remove_tags
+    params[:task][:tags].each do |tag|
+      if Tag.exists?(name: tag[:name])
+        if tag[:remove] == '1'
+          @task.tags.destroy(Tag.find_by(name: tag[:name]))
+        elsif !@task.tags.exists?(name: tag[:name])
+          @task.tags << Tag.find_by(name: tag[:name])
+        end
+      else
+        @task.tags.create name: tag[:name]
+      end
+    end
+  end
+
 end
