470fb6093caceff787ba0cf1bd4bf4668da76641
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-01-03 11:46:55 +0800

    rename variable and reformat

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,21 +1,21 @@
 module TasksHelper
   # add a flash reminder message if there are urgent tasks
   def add_reminder_for_urgent
-    _urgent_count = Task.count_urgent
-    if _urgent_count.positive?
-      flash.now[:reminder] = "#{_urgent_count}
-      #{_urgent_count == 1 ? 'task is' : 'tasks are'}
-      due within one day."
+    urgent_count = Task.count_urgent
+    if urgent_count.positive?
+      flash.now[:reminder] = "#{urgent_count}
+        #{urgent_count == 1 ? 'task is' : 'tasks are'}
+        due within one day."
     end
   end
 
   # add a flash alert message if there are overdue tasks
   def add_alert_for_overdue
-    _overdue_count = Task.count_overdue
-    if _overdue_count.positive?
-      flash.now[:alert] = "#{_overdue_count}
-      #{_overdue_count == 1 ? 'task is' : 'tasks are'}
-      overdue"
+    overdue_count = Task.count_overdue
+    if overdue_count.positive?
+      flash.now[:alert] = "#{overdue_count}
+        #{overdue_count == 1 ? 'task is' : 'tasks are'}
+        overdue"
     end
   end
 
