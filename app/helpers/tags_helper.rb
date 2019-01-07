30ba243df032acae003989771a4805f920c4417d
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-01-07 22:54:00 +0800

    refactor link helpers out into a separate file

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,15 +1,4 @@
 module TagsHelper
-  # generate a link to dynamically add a tag when editing a task
-  def link_to_add_tag
-    fields = content_tag :div, class: 'tag' do
-      text_field_tag 'task[tags][][name]'
-    end
-    link_to 'Add Tag',
-            '#',
-            id: 'add_tag',
-            data: { fields: fields.delete("\n") }
-  end
-
   # add or remove tags as required
   def add_or_remove_tags(task)
     if params[:task][:tags].respond_to? 'each'
@@ -25,20 +14,6 @@ module TagsHelper
         end
       end
     end
-  end
-
-  # generate a link to tags#index
-  def link_to_tags_path
-    link_to 'Manage Tags', tags_path
-  end
-
-  # generate a link to delete tags
-  def link_to_delete_tag(tag, display)
-    link_to display,
-            tag_path(tag),
-            method: :delete,
-            data: { confirm: 'Sure to delete this tag? '\
-              'This does not delete its tasks. ' }
   end
 
 end
