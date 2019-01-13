a6d0e44f939cd7d36cfb3c9cee9720618202e99d
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-01-14 02:25:38 +0800

    UI tweaks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  # last TODO: remove redundant files & functions, and we'll be ready for final deployment
   # get tasks by category
   def index
     if params[:new_user] == '1'
